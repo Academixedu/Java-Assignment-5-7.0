236a4306604e8ba23d17185ff70420a1e3faf7bd
2110Abhishek <agchoudhari2110@gmail.com>
2024-07-22 21:30:00 +0530

    Abhishek

--- Loops.java
+++ Loops.java
@@ -1,3 +1,27 @@
-public class Loops{
-// Prepare a For Loop and While  in each Function
+public class Loops
+{
+   public void method1()
+   {
+    for (int i=0;i<=10;i++)
+    {
+        System.out.println("Abhishek = " + i);
+    }
+    
+   }
+      public void method2()
+      {
+        int a=11;
+        while (a<=20) 
+        {
+            System.out.println("Choudhari = "+a);
+            a++;
+        }
+      }
+   public static void main(String[] args) 
+   {
+    new Loops().method1();
+    new Loops().method2();
+    
+   }
+
 }
