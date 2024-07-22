95bf071a45b2e4cc6ddbfbc0a66876239abbb5e0
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-22 16:12:18 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
 // Scanner Implementations
+public class main {
+    public static void main(String[] args) {
+        personclass person = new personclass("Bharath", 22, "Ameerpe", 143);
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+        System.out.println(person.getAddres());
+        System.out.println(person.getId());
+    }
+}
