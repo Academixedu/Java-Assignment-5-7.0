5fc000f84432a8b4f4b77767c65ab226930fbe62
ratan-17 <rkmule2002@gmail.com>
2024-07-22 16:29:26 +0530

    Ratan

--- IfElse.java
+++ IfElse.java
@@ -1,4 +1,36 @@
-public class IfElse{
+import java.util.Scanner;
+
+public class IfElse
+{
 //Take a variable and scan value using Scanner
   // Make Conditions using if else
+
+  Scanner sc= new Scanner(System.in);
+   void meth1()
+   {
+       System.out.print("Enter the value of a :");
+       int a=sc.nextInt();
+
+       System.out.print("Enter the value of b :");
+       int b=sc.nextInt();
+
+       if (a<b)
+       {
+        System.out.println("The Value of b is greater : "+b);
+       }
+       else if(a>b)
+       {
+        System.out.println("The value of a is greater : "+a);
+       }
+       else
+       {
+        System.out.println("Both the Values of a & b is equal : a="+a+" b="+b);
+       }
+    
+   }
+  public static void main(String[] args) 
+  {
+     IfElse obj=new IfElse();
+     obj.meth1();
+  }
 }
--- ParentClass.java
+++ ParentClass.java
@@ -1,3 +1,25 @@
-public class ParentClass{
+public class ParentClass
+{
   // prepare a method Name with name as parameter and return it by using String return type
+
+  public String meth3()
+  {
+
+   return new Basic().meth4(" is awesome");
+  }
+
+  public String meth4(String s)
+  {
+   return s;
+  }
+
+  public static void main(String[] args)
+   {
+      ParentClass obj=new ParentClass();
+    System.out.println("Java"+ obj.meth3());
+  }
+
+
+
+ 
 }
