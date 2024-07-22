865d2ac2ce5bcb4b484f901a0c9a0c4a94ed779e
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-22 15:28:21 +0530

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -1,3 +1,22 @@
 public class Loops{
 // Prepare a For Loop and While  in each Function
 }
+public class Loops {
+    public static void main(String[] args) {
+     int a,b;
+     a=2;
+     b=4;
+     for (int i = 0; i < 10; i++) {
+        System.out.println(a);
+         
+     }
+     int counter=0;
+     while (counter<b) {
+        System.out.println(b);
+        counter++; 
+         
+     }
+
+    }
+    
+}
