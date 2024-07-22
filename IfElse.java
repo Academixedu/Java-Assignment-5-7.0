e82459e32b614af5989d9f18e8bfe71d05291d24
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-07-22 16:21:06 +0530

    MANI

--- IfElse.java
+++ IfElse.java
@@ -1,4 +1,39 @@
-public class IfElse{
+
+
+
+
+public class IfElse{    
+  
 //Take a variable and scan value using Scanner
   // Make Conditions using if else
+  /**
+   * 
+   */
+  public static void f1(int age){
+
+  
+  if(age < 18)
+  {
+    System.out.println("YOU ARE NOT ELIGIBLE TO ACCESS THIS WEBSITE");
+  }
+  else
+  {
+    System.out.println("YOU ARE ELIGIBLE TO ACCESS THIS WEBSITE");
+  }
+  }
+
+  
 }
+
+
+
+
+
+
+
+
+
+  
+
+
+
--- Loops.java
+++ Loops.java
@@ -1,3 +1,27 @@
 public class Loops{
 // Prepare a For Loop and While  in each Function
+public static int forLoop(int a)
+{
+    for(int i=0;i<a;i++)
+    {
+        System.out.println("i=="+i);
+    }
+    return a;
 }
+
+public static int whileLoop(int b)
+{   
+    int count = 0;
+    while(count < b)
+    {
+        System.out.println("count=="+count);
+        count++;
+    }
+    return b;
+
+}
+
+
+
+}
+
