868c77479af596d96cb5775c4424d17e0178da1e
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-07-22 14:22:58 +0530

    main

--- Main.java
+++ Main.java
@@ -1,1 +1,38 @@
 // Scanner Implementations
+public class Main{
+
+
+
+
+
+public static void main(String args[]){
+
+
+
+  Person p=new Person("Pavani",22,20000,"developer");
+  System.out.println(p.getAge());
+  System.out.println(p.getName());
+  System.out.println(p.getSal());
+  System.out.println(p.getDesg());
+  p.setAge(22);
+  p.setName("ganesh");
+  p.setSal(30000);
+  System.out.println(p.getAge());
+  System.out.println(p.getName());
+  System.out.println(p.getSal());
+  
+
+
+
+
+  
+
+
+}
+}
+
+    
+    
+
+
+
