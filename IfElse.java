7402ce7ffa5885dfd693198bb7f305102db023b7
syamkarthik0 <113086856+syamkarthik0@users.noreply.github.com>
2024-07-22 17:19:27 +0530

    Update IfElse.java

--- IfElse.java
+++ IfElse.java
@@ -1,4 +1,18 @@
-public class IfElse{
-//Take a variable and scan value using Scanner
-  // Make Conditions using if else
+import java.util.Scanner;
+
+public class IfElse {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter a number: ");
+    int num = sc.nextInt();
+
+    if (num > 10) {
+      System.out.println("The number is greater than 10");
+    } else if (num == 10) {
+      System.out.println("The number is equal to 10");
+    } else {
+      System.out.println("The number is less than 10");
+    }
+  }
 }
