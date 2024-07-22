ddcf5fa006a0f54311a50e2fa4a5fc4355e51319
syamkarthik0 <113086856+syamkarthik0@users.noreply.github.com>
2024-07-22 17:33:48 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,26 @@
-// Scanner Implementations
+import java.util.Scanner;
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int integerInput = sc.nextInt();
+        System.out.println("Entered integer: " + integerInput);
+
+        System.out.print("Enter a float number: ");
+        float floatInput = sc.nextFloat();
+        System.out.println("Entered float: " + floatInput);
+      
+        System.out.print("Enter a double number: ");
+        double doubleInput = sc.nextDouble();
+        System.out.println("Entered double: " + doubleInput);
+
+        sc.nextLine(); 
+        System.out.print("Enter a string: ");
+        String stringInput = sc.nextLine();
+        System.out.println("Entered string: " + stringInput);
+
+        System.out.print("Enter true or false: ");
+        boolean booleanInput = sc.nextBoolean();
+        System.out.println("Entered boolean: " + booleanInput);
+    }
+}
