5d59a78f6836b815245cad1e0ee11d40cc479bbd
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-07-22 23:56:40 +0530

    perent

--- ParentClass.java
+++ ParentClass.java
@@ -1,3 +1,8 @@
 public class ParentClass{
   // prepare a method Name with name as parameter and return it by using String return type
+  public String surname(String name){
+    System.out.println("Behera");
+    return name;
+    
+  }
 }
