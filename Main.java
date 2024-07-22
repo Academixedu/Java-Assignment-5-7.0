cf88c79b3fe937443284a3b213226d058bf31646
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-07-22 23:55:56 +0530

    changes

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
-// Scanner Implementations
+public class Main{
+    public static void main(String [] args){
+        Person n = new Person("fayaz",245,21,"Berhampur");
+        System.out.println("name is "+n.getName());
+        System.out.println("id is "+n.getId());
+        System.out.println("age is "+n.getAge());
+        System.out.println(" role is "+n.getAddress());
+        Child pc = new Child();
+        pc.name("Ritesh");
+        pc.surname("Behera");
+    }
+}
--- Person.java
+++ Person.java
@@ -1,5 +1,42 @@
+import java.util.jar.Attributes.Name;
+
 public class Person{
   // Take 4 Variables and Initialize them
-  // Create Getters and Setters
+  public String name;
+  public int id;
+  private int age;
+  private String address;
   // Create Constructors
+  public Person(String name, int id, int age, String address){
+    this.name = name;
+    this.id= id;
+    this.age = age;
+    this.address = address;
+  }
+   // Create Getters and Setters
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+  public String getAddress() {
+    return address;
+  }
+  public void setAddress(String address) {
+    this.address = address;
+  }
+  
 }
