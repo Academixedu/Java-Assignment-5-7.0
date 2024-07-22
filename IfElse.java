091271a7b8e20f8c22b8efa202da11c56fee5853
ganeshteki <ganeshteki2001@gmail.com>
2024-07-22 14:31:50 +0530

    ie

--- IfElse.java
+++ IfElse.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 public class IfElse{
 //Take a variable and scan value using Scanner
-  // Make Conditions using if else
+//public static void main(String args[]){
+  //Scanner sc =new Scanner (System.in);
+  //System.out.println("enter a number");
+  //int a=sc.nextInt();
+ // Make Conditions using if else
+ public static void ife(int a)
+ {
+ if (a>10)
+ {
+  System.out.println("a value >10");
+ }
+ else{
+  System.out.println("<10");
+ }
 }
+}
+
