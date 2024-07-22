2fd1ca6298e9e45a2316374b18ba7f4973c9bef1
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-07-22 22:58:50 +0530

    ifelse

--- IfElse.java
+++ IfElse.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class IfElse{
 //Take a variable and scan value using Scanner
-  // Make Conditions using if else
+public static void main(String[] args) {
+  int a, b;
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter a: ");
+  a = scan.nextInt();
+
+  Scanner Scan = new Scanner(System.in);
+  System.out.println("Enter b: ");
+  b = scan.nextInt();
+  if (a>b) {
+    System.out.println("a id grater then b");
+    
+  }
+  else{
+    System.out.println("B is grater then a");
+  }
+
+}
+  // Make Conditions using if else 
+
 }
