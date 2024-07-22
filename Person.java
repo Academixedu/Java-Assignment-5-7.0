42da330a839855116650248d285abae43343dd9a
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-22 16:14:16 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -3,3 +3,48 @@ public class Person{
   // Create Getters and Setters
   // Create Constructors
 }
+public class personclass extends parentclass {
+    public String name;
+    public int age;
+    private String addres;
+    private int id;
+    public personclass(String name, int age, String addres, int id) {
+        this.name = name;
+        this.age = age;
+        this.addres = addres;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
