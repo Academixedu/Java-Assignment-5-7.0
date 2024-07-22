4c5f49c7e8c5282d131f28bc5afe70cb738320be
ganeshteki <ganeshteki2001@gmail.com>
2024-07-22 14:31:38 +0530

    fun

--- Functions.java
+++ Functions.java
@@ -1,10 +1,25 @@
 public class Functions{
 // prepare a void function
-  // prepare a function with parameters 
+public void sum(){
+  System.out.println("sum of two integers");
+}
+  // prepare a function with parameters
+  public void sum(int a,int b)
+  {
+    int c=a+b;
+    System.out.println("sum="+c);
+  } 
+  public String sum(String name)
+  {
+    return name;
+  }
   // prepare a function using paramater and returntype
   // 3 methods should contain same name
   public static void main(String[]args){
   Functions f=new Functions();
   // Call three Functions here
+  f.sum();
+  f.sum(2,3);
+  System.out.println(f.sum("gani"));
   }
 }
