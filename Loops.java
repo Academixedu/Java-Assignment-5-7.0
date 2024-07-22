ad194fe8305701ccff191b995dde22a8a1ce885a
fayazshaik515 <fayazshaik231104@gmail.com>
2024-07-22 14:25:41 +0530

    commit

--- Loops.java
+++ Loops.java
@@ -1,3 +1,18 @@
 public class Loops{
+    public  static int   loop(int a ){ 
+        for(int i=a;i<=5;i++){
+            System.out.println("fayaz");
+        }
+        return a;
+    }
+    public static int While(int i){
+        i = 0;
+        while(i<=5){
+        System.out.println("sai");
+        i++;        
+    }
+    return i;
+
+    }
 // Prepare a For Loop and While  in each Function
 }
