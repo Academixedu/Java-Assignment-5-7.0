d89e446538284c3a8eef125e60a95bdbecdf078d
ganeshteki <ganeshteki2001@gmail.com>
2024-07-22 14:32:03 +0530

    lo

--- Loops.java
+++ Loops.java
@@ -1,3 +1,18 @@
 public class Loops{
 // Prepare a For Loop and While  in each Function
+public static void For(int a){
+    for(int i=0;i<=a;i++)
+    {
+        System.out.println(i);
+    }
+}
+    public static void While(int a)
+    {
+        int count=a;
+        while(count>=0)
+        {
+            System.out.println(count);
+            count--;
+        }
+    }   
 }
