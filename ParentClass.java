0b8955fe3333132e8156be77602766f34f5b35c9
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-07-22 14:23:52 +0530

    parent

--- ParentClass.java
+++ ParentClass.java
@@ -1,3 +1,13 @@
 public class ParentClass{
+  public String surname(String name){
+    return name;
+  }
   // prepare a method Name with name as parameter and return it by using String return type
+
+public static void main(String args[]){
+  ParentClass p=new ParentClass();
+  System.out.println(p.surname("pavani"));
+
+
+}
 }
