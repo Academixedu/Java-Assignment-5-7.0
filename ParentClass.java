e9151d4c946f1aede59d6f4e73acbb6a35bf93b8
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-22 16:13:25 +0530

    Update ParentClass.java

--- ParentClass.java
+++ ParentClass.java
@@ -1,3 +1,9 @@
 public class ParentClass{
   // prepare a method Name with name as parameter and return it by using String return type
 }
+public class parentclass {
+    public void name() {
+        System.out.println("name");
+    }
+    
+}
